37529def196dc848881ca860107fc248f30004bc
Aniket Chitale <aniketchitale7@gmail.com>
2016-11-06 01:24:09 -0600

    Changes for Login signup and Email

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_secure_password
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable , :confirmable, :lockable
+end
