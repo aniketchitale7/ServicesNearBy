b0160b77dfcfc0549c6a864ac3d7475d19764957
Aniket <aniketchitale7@gmail.com>
2016-11-06 20:47:31 +0000

    Production changes for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,11 +55,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "blog_#{Rails.env}"
-  config.action_mailer.perform_caching = false
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+   
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -83,4 +79,25 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.perform_caching = true
+
+  # Ignore bad email addresses and do not raise email delivery errors.
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  config.action_mailer.raise_delivery_errors = true
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host:'morning-beach-21493.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
+  #config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => 'morning-beach-21493.herokuapp.com',
+      :user_name => "servicesnearby@gmail.com",
+      :password => "oakcrest1100",
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
 end
