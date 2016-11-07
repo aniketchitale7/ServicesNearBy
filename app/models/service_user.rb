2131defeca353c1192764f34d3085f9e74de7c8f
skalyanmoguloju <kalyansaim@gmail.com>
2016-11-06 23:41:56 -0600

    Info Screen pull

--- app/models/service_user.rb
+++ app/models/service_user.rb
@@ -1,13 +1,8 @@
 class Service_User < ActiveRecord::Base
   def self.create_service_user(user_params)
-    begin
-      random_token = SecureRandom.urlsafe_base64(nil, false)
-      first_truck = Service_User.select(:user_session_token).find_by_session_token(random_token) # => <Truck id: nil, truck_no: "123"> OR nil if no record matches criteria
-      if first_truck == nil
-        user_params[:user_session_token] = random_token
-        break
-      end
-    end while true
+    random_token = SecureRandom.urlsafe_base64(nil, false)
+    user_params[:user_session_token] = random_token
     Service_User.create!(user_params)
+    puts "Ssfs"
   end
 end
