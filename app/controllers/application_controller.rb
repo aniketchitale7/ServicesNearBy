d9bc83448ddfe3f78d50f5ca85edd6a4bfc986a5
skalyanmoguloju <kalyansaim@gmail.com>
2016-11-16 16:43:27 -0600

    Admin Screens Initial Template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,17 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(user)
     puts user.email
     @service_user = Service_User.find_by_user_email(user.email)
+    session[:logged_user] = @service_user
     if @service_user == nil
+      session[:roleid] = -1
       puts "NILL"
       session[:user_emailid] = user.email
       myprofile_myprofile_path
     else
+      session[:roleid] = @service_user.service_roles_id
       puts @service_user.service_roles_id
       if @service_user.service_roles_id ==0
-        welcome_admin_path
+        admin_index_path
       else
         welcome_index_path
       end
