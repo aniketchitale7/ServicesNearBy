6432fad929d440836af001560dcfe12985baa0bc
Ankur Shrivastava <ankur-shrivastava@uiowa.edu>
2016-11-20 18:43:43 -0600

    updating profile data

--- app/controllers/myprofile_controller.rb
+++ app/controllers/myprofile_controller.rb
@@ -1,12 +1,14 @@
-
 class MyprofileController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
-  def service_user_params
+  def index
 
-
-      params.require(:user_firstname)
   end
+  def service_user_params
+    params.require(:user_firstname)
+  end
+
+
   def new
     puts "testing"
   end
@@ -14,16 +16,21 @@ class MyprofileController < ApplicationController
 
   def create
 
+    puts session[:user_emailid]
+    puts session[:logged_user]
+    @hashe = session[:logged_user]
+    puts session[:roleid]
     @user_service = Service_User.new
-    my_hash = {'user_firstname' => params.require(:user_firstname),
+    my_hash = {'user_name' => "Dummy",
+               'user_firstname' => params.require(:user_firstname),
                'user_lastname' => params.require(:user_lastname),
                'user_phone' => params.require(:user_phone),
                'service_roles_id' => params.require(:service_roles_id),
-               'user_email' => "DUMMY",
+               'user_email' => session[:user_emailid],
                'user_password' => "DUMMY",
                'user_name' => "testing",
-               'created_at' => "DUMMY",
-               'updated_at' => "DUMMY"}
+               'created_at' => "25-Nov-1992",
+               'updated_at' => "25-Nov-1992"}
     puts my_hash
     # @user_service.user_firstname = params.require(:user_firstname)
     # @user_service.user_lastname = params.require(:user_lastname)
@@ -34,8 +41,21 @@ class MyprofileController < ApplicationController
     # @user_service.user_password = "dummy"
     # @user_service.created_at = "25-Nov-1992"
     # @user_service.updated_at = "25-Nov-1992"
-    @user_new = Service_User.create_service_user(my_hash)
-    # @user_new = Service_User.create(my_hash)
+
+    @user = Service_User.find_by_user_email(session[:user_emailid])
+    puts 'inside create'
+    puts @user.service_roles_id
+    session[:userinfohash] =my_hash
+    #session[:lastname] = @service_user.user_lastname
+
+    if @user.service_roles_id == -1
+      @user_new = Service_User.create_service_user(my_hash)
+      redirect_to welcome_index_path
+    else
+      @user_new = Service_User.update_service_user(my_hash)
+      redirect_to welcome_index_path
+      # @user_new = Service_User.create(my_hash)
+    end
   end
 
 
