e36b190a0f1d9d35500e559fd7c9bced4826bed4
chanakya kumar <chanakya@Chanakyas-MacBook-Pro.local>
2016-12-05 00:55:11 -0600

    Revert "Revert "Revert "Merge branch 'master' into branch_Chanakya"""
    This reverts commit 2b9da10f74a3c08392b533688801ef2242c8793f.

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
-  #get 'myprofile/myprofile'
+  get 'myprofile/myprofile'
   root 'welcome#index'
-<<<<<<< HEAD
   get 'service_services/index'
   post 'myprofile/create'
   get 'welcome/admin'
-=======
-  post 'application/search'
-  post 'welcome/search'
-  get 'myprofile/requestvendoraccount' , :to => 'myprofile#requestvendoraccount', :as => 'requestvendoraccount'
-  get 'myprofile/createVendorAccount'
-  resources :myprofile
-  resources :businessprofile
-  get 'myprofile/update'
-  #post 'myprofile/create'
-  #get 'myprofile/edit'
-  get 'admin/index'
-  get 'search/index'
-
-  resources :admin
-
-  get '/admin/reject/:id', :to => 'admin#reject', :as => 'adminreject'
-  get '/admin/activate/:id', :to => 'admin#activate', :as => 'adminactivate'
-  get '/admin/deactivate/:id', :to => 'admin#deactivate', :as => 'admindeactivate'
-  get '/admin/deactivatecategory/:id', :to => 'admin#deactivatecategory', :as => 'admindeactivatecategory'
-  get '/admin/activatecategory/:id', :to => 'admin#activatecategory', :as => 'adminactivatecategory'
->>>>>>> master
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
