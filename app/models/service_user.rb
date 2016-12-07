36dfe37975b0ddad8d1fbd19ca467e060e8bef42
Aniket Chitale <aniketchitale7@gmail.com>
2016-12-07 15:44:03 -0600

    Fixing Rspec Error

--- app/models/service_user.rb
+++ app/models/service_user.rb
@@ -10,14 +10,8 @@ class ServiceUser < ActiveRecord::Base
   end
 
   def self.update_service_user(user_params)
-    puts "00000"
-    puts user_params
     user = Service_User.find_by_user_email(user_params["user_email"])
-    puts user_params["user_email"]
-    puts 'inside update'
-    puts user_params
     user.update_attributes(user_params)
-    puts "update"
   end
 
 end
