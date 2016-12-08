dbb2396eafb74dd3d04f2c6599cbf3e1b3080c04
skalyanmoguloju <kalyansaim@gmail.com>
2016-12-08 03:38:24 -0600

    Completed application spec

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,38 +11,80 @@ service_role = [{:role_name => 'Admin' , :role_description => 'Admin'  } ,
                {:role_name => 'User' , :role_description => 'User' }]
 
 
-# service_address = [{:address_line1 => '1100 Oakcrest Street', :address_line2 => 'Apt B', :address_zip => '52246',
-#                    :address_city => "Iowa City" ,
-#                    :address_state => "IA" ,:address_country => "USA" ,:address_lattitute => 41.6297493 ,
-#                    :address_longitude => -91.6109434} ,
-#
-#                   {:address_line1 => '1100 Oakcrest Street', :address_line2 => 'Apt C', :address_zip => '52246',
-#                    :address_city => "Iowa City" ,
-#                    :address_state => "IA" ,:address_country => "USA" ,:address_lattitute => 41.6297493 ,
-#                    :address_longitude => -91.6109434} ,
-#
-#                   {:address_line1 => '1100 Oakcrest Street', :address_line2 => 'Apt D', :address_zip => '52246',
-#                    :address_city => "Iowa City" ,
-#                    :address_state => "IA" ,:address_country => "USA" ,:address_lattitute => 41.6297493 ,
-#                    :address_longitude => -91.6109434}
-# ]
+service_address = [{:address => '1100 Oakcrest Street', :landmark => 'Apt B',:address_lattitute => 41.6297493 ,
+                   :address_longitude => -91.6109434} ,
+
+                  {:address => '1100 Oakcrest Street', :landmark => 'Apt C', :address_lattitute => 41.6297493 ,
+                   :address_longitude => -91.6109434} ,
+
+                  {:address => '1100 Oakcrest Street', :landmark => 'Apt D',:address_lattitute => 41.6297493 ,
+                   :address_longitude => -91.6109434}
+]
 
 
-# service_user = [{:user_firstname => 'Services' ,
-#                  :user_lastname => 'NearMe' , :user_phone => '3192375499' ,
-#                  :user_email => 'servicesnearby@gmail.com' , :user_info => 'I am Awesome' , :user_status => 'Active',
-#                  :service_role_id => 2, :service_address_id => 1},
-#
-#                 { :user_firstname => 'Ankur' ,
-#                  :user_lastname => 'Shrivastava' , :user_phone => '3192375499' ,
-#                  :user_email => 'shrivastava.ankur1190@gmail.com' , :user_info => 'I am Dude' , :user_status => 'Active',
-#                  :service_role_id => 3, :service_address_id => 2},
-#
-#                 {:user_firstname => 'Ajinkya' ,
-#                  :user_lastname => 'Kulkurni' , :user_phone => '3192375499' ,
-#                  :user_email => 'ajinkya.kulkarni1102@gmail.com' , :user_info => 'I am Madrid' , :user_status => 'Active',
-#                  :service_role_id => 1, :service_address_id => 3}
-#     ]
+
+user = [{:email => 'user.inactive@gmail.com', :password => 'kanna91221'},
+        {:email => 'user.active@gmail.com', :password => 'kanna91221'},
+        {:email => 'admin.inactive@gmail.com', :password => 'kanna91221'},
+        {:email => 'admin.active@gmail.com', :password => 'kanna91221'},
+        {:email => 'vendor.active@gmail.com', :password => 'kanna91221'},
+        {:email => 'vendor.inactive@gmail.com', :password => 'kanna91221'},
+        {:email => 'user.pending@gmail.com', :password => 'kanna91221'},
+        {:email => 'first.time@gmail.com', :password => 'kanna91221'}
+]
+
+
+service_user =                   [{:user_firstname => 'Services' ,
+                           :user_lastname => 'NearMe' , :user_phone => '3192375499' ,
+                           :user_email => 'servicesnearby@gmail.com' , :user_info => 'I am Awesome' , :user_status => 'Active',
+                           :service_role_id => 2, :service_address_id => 1},
+
+                          { :user_firstname => 'Ankur' ,
+                           :user_lastname => 'Shrivastava' , :user_phone => '3192375499' ,
+                           :user_email => 'shrivastava.ankur1190@gmail.com' , :user_info => 'I am Dude' , :user_status => 'Active',
+                           :service_role_id => 3, :service_address_id => 2},
+
+                          {:user_firstname => 'Ajinkya' ,
+                           :user_lastname => 'Kulkurni' , :user_phone => '3192375499' ,
+                           :user_email => 'ajinkya.kulkarni1102@gmail.com' , :user_info => 'I am Madrid' , :user_status => 'Active',
+                           :service_role_id => 1, :service_address_id => 3},
+
+                           {:user_firstname => 'Admin' ,
+                            :user_lastname => 'Active' , :user_phone => '3192375499' ,
+                            :user_email => 'admin.active@gmail.com' , :user_info => 'I am Madrid' , :user_status => 'Active',
+                            :service_role_id => 1, :service_address_id => 1},
+
+                           {:user_firstname => 'Admin' ,
+                            :user_lastname => 'Inactive' , :user_phone => '3192375499' ,
+                            :user_email => 'admin.inactive@gmail.com' , :user_info => 'I am Madrid' , :user_status => 'Inactive',
+                            :service_role_id => 1, :service_address_id => 1},
+
+                           {:user_firstname => 'User' ,
+                            :user_lastname => 'Active' , :user_phone => '3192375499' ,
+                            :user_email => 'user.active@gmail.com' , :user_info => 'I am Madrid' , :user_status => 'Active',
+                            :service_role_id => 3, :service_address_id => 1},
+
+                           {:user_firstname => 'User' ,
+                            :user_lastname => 'Inactive' , :user_phone => '3192375499' ,
+                            :user_email => 'user.inactive@gmail.com' , :user_info => 'I am Madrid' , :user_status => 'Inactive',
+                            :service_role_id => 3, :service_address_id => 1},
+
+                           {:user_firstname => 'Vendor' ,
+                            :user_lastname => 'Active' , :user_phone => '3192375499' ,
+                            :user_email => 'vendor.active@gmail.com' , :user_info => 'I am Madrid' , :user_status => 'Active',
+                            :service_role_id => 2, :service_address_id => 1},
+
+                           {:user_firstname => 'Vendor' ,
+                            :user_lastname => 'Inactive' , :user_phone => '3192375499' ,
+                            :user_email => 'vendor.inactive@gmail.com' , :user_info => 'I am Madrid' , :user_status => 'Inactive',
+                            :service_role_id => 2, :service_address_id => 1},
+
+                           {:user_firstname => 'User' ,
+                            :user_lastname => 'Pending' , :user_phone => '3192375499' ,
+                            :user_email => 'user.pending@gmail.com' , :user_info => 'I am Madrid' , :user_status => 'Pending',
+                            :service_role_id => 3, :service_address_id => 1}
+
+                     ]
 
 
 service_category = [{:service_name => 'Repairs',
@@ -58,16 +100,31 @@ service_category = [{:service_name => 'Repairs',
 
 
 
-# services_ser = [{:service_name => 'Stella Home Decore', :service_address_id => 1, :service_category_id => 2, :service_user_id => 1,
-#                  :phoneNo => '31933454', :keywords => 'Home Decore,Painting,Wall Painting,Design,Furniture,wallpaper' , :service_time => 'Monday - Saturday 11:00 - 9:00',
-#                  :service_price => 20.0,
-#                  :service_status => 'Active' , :service_description => 'Home Furnishing Stores, Furniture Stores'
-#                 } ,
-#                 {:service_name => 'Everything Works', :service_address_id => 2, :service_category_id => 1, :service_user_id => 1,
-#                  :phoneNo => '31933454', :keywords => 'repairs,solder,computer,ac,electronics,laptop' , :service_time => 'Monday - Saturday 11:00 - 9:00',
-#                  :service_price => 40.0,
-#                  :service_status => 'Active' , :service_description => 'This include repair of Computers, Air Conditioners, Dryers, Televisions, Refrigerators, Others',
-#                 }]
+services_ser = [{:service_name => 'Stella Home Decore', :service_address_id => 1, :service_category_id => 2, :service_user_id => 1,
+                 :phoneNo => '31933454', :keywords => 'Home Decore,Painting,Wall Painting,Design,Furniture,wallpaper' , :service_time => 'Monday - Saturday 11:00 - 9:00',
+                 :service_price => 20.0,
+                 :service_status => 'Active' , :service_description => 'Home Furnishing Stores, Furniture Stores'
+                } ,
+                {:service_name => 'Everything Works', :service_address_id => 2, :service_category_id => 1, :service_user_id => 1,
+                 :phoneNo => '31933454', :keywords => 'repairs,solder,computer,ac,electronics,laptop' , :service_time => 'Monday - Saturday 11:00 - 9:00',
+                 :service_price => 40.0,
+                 :service_status => 'Active' , :service_description => 'This include repair of Computers, Air Conditioners, Dryers, Televisions, Refrigerators, Others',
+                },
+                {:service_name => 'Active Service', :service_address_id => 2, :service_category_id => 1, :service_user_id => 1,
+                 :phoneNo => '31933454', :keywords => 'repairs,solder,computer,ac,electronics,laptop' , :service_time => 'Monday - Saturday 11:00 - 9:00',
+                 :service_price => 40.0,
+                 :service_status => 'Active' , :service_description => 'This include repair of Computers, Air Conditioners, Dryers, Televisions, Refrigerators, Others',
+                },
+                {:service_name => 'Inactive Service', :service_address_id => 2, :service_category_id => 1, :service_user_id => 1,
+                 :phoneNo => '31933454', :keywords => 'repairs,solder,computer,ac,electronics,laptop' , :service_time => 'Monday - Saturday 11:00 - 9:00',
+                 :service_price => 40.0,
+                 :service_status => 'Inactive' , :service_description => 'This include repair of Computers, Air Conditioners, Dryers, Televisions, Refrigerators, Others',
+                },
+                {:service_name => 'Pending Service', :service_address_id => 2, :service_category_id => 1, :service_user_id => 1,
+                 :phoneNo => '31933454', :keywords => 'repairs,solder,computer,ac,electronics,laptop' , :service_time => 'Monday - Saturday 11:00 - 9:00',
+                 :service_price => 40.0,
+                 :service_status => 'Pending' , :service_description => 'This include repair of Computers, Air Conditioners, Dryers, Televisions, Refrigerators, Others',
+                }]
 
 
 
@@ -76,23 +133,27 @@ service_role.each do |serviceadd|
   ServiceRole.create!(serviceadd)
 end
 
-# service_address.each do |serviceadd|
-#   ServiceAddress.create!(serviceadd)
-# end
+service_address.each do |serviceadd|
+  ServiceAddress.create!(serviceadd)
+end
 
 service_category.each do |servicecat|
   ServiceCategory.create!(servicecat)
 end
 
-# service_user.each do |user|
-#   ServiceUser.create!(user)
-# end
+service_user.each do |user|
+  ServiceUser.create!(user)
+end
 
 
-# services_ser.each do |user|
-#   ServiceService.create!(user)
-# end
+services_ser.each do |user|
+  ServiceService.create!(user)
+end
 
 
+
+user.each do |user|
+  User.create!(user)
+end
 
 
--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -8,14 +8,61 @@ RSpec.describe ApplicationController, type: :controller do
 
 
   describe "After sigin-in" do
-    it "redirects to the root path" do
+    it "redirects to the admin path" do
       session[:logged_user] = nil
       session[:roleid] = nil
       session[:user_emailid] = nil
       session[:loggedUserAddress] = nil
-      @user = User.find_by_email("kalyansaim@gmail.com")
+      @user = User.find_by_email("admin.active@gmail.com")
       controller.after_sign_in_path_for(@user)
     end
+
+    it "redirects to the user path" do
+      session[:logged_user] = nil
+      session[:roleid] = nil
+      session[:user_emailid] = nil
+      session[:loggedUserAddress] = nil
+      @user = User.find_by_email("user.active@gmail.com")
+      controller.after_sign_in_path_for(@user)
+    end
+
+
+    it "redirects to the vendor path" do
+      session[:logged_user] = nil
+      session[:roleid] = nil
+      session[:user_emailid] = nil
+      session[:loggedUserAddress] = nil
+      @user = User.find_by_email("vendor.active@gmail.com")
+      controller.after_sign_in_path_for(@user)
+    end
+
+    it "inactive user login" do
+      session[:logged_user] = nil
+      session[:roleid] = nil
+      session[:user_emailid] = nil
+      session[:loggedUserAddress] = nil
+      @user = User.find_by_email("vendor.inactive@gmail.com")
+      controller.after_sign_in_path_for(@user)
+    end
+    it "inactive admin login" do
+      session[:logged_user] = nil
+      session[:roleid] = nil
+      session[:user_emailid] = nil
+      session[:loggedUserAddress] = nil
+      @user = User.find_by_email("admin.inactive@gmail.com")
+      controller.after_sign_in_path_for(@user)
+    end
+
+
+    it "first time user login" do
+      session[:logged_user] = nil
+      session[:roleid] = nil
+      session[:user_emailid] = nil
+      session[:loggedUserAddress] = nil
+      @user = User.find_by_email("first.time@gmail.com")
+      controller.after_sign_in_path_for(@user)
+    end
+
   end
 
 end
