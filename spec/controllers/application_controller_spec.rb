2d7b0f0d9ffbd3c26118a1879119a5d7476c6fb1
skalyanmoguloju <kalyansaim@gmail.com>
2016-12-08 01:38:23 -0600

    Fix in application spec

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,15 +2,6 @@ require 'spec_helper'
 require 'rails_helper'
 RSpec.describe ApplicationController, type: :controller do
 
-  describe "Login sequence" do
-    it "check which user has logged in" do
-     user = User.new(
-     email: 'kalyansim@gmail.com')
-
-      post :after_sign_in_path_for(user)
-      expect(response).to render_template("driverdetails/new")
-    end
-  end
 
 
 
